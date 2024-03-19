1f1f7f2a56bb5e2169128681d1c7bb787954faea
Júlio Jaszumbek <juliojaszumbek98@hotmail.com>
2024-03-18 21:25:25 -0300

    Destruir Os Canos
    Alteração para destruir os canos após passarem o pássaro.

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -13,5 +13,10 @@ public class Pipe : MonoBehaviour
     void Update()
     {
         transform.position += Vector3.left * Time.deltaTime * speed;
+
+        if (transform.position.x < -2) 
+        {
+            Destroy(gameObject);
+        }
     }
 }
